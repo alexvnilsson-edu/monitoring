e71acf1456d5120c4d911bd933db2156f44620f3
Alexander Nilsson <alexvnilsson@outlook.com>
2020-03-10 15:58:02 +0100

    refactor(runas.sh): rename run to runas, add run
    run.sh: run main.py with python3
    runas.sh: run main.py with python3 as user monitoring

--- runas.sh
+++ runas.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+user=monitoring
 python=`which python3`
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 MAIN="$SCRIPTPATH/main.py"
 
-$python $MAIN
+runuser -l $user -c "$python $MAIN" 
