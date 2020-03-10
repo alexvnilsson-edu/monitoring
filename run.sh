0f80b72645215588b1380a854069c6257e3a45cf
Alexander Nilsson <alexvnilsson@outlook.com>
2020-03-10 16:07:50 +0100

    refactor(run): add logging to run-script

--- run.sh
+++ run.sh
@@ -4,4 +4,6 @@ python=`which python3`
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 MAIN="$SCRIPTPATH/main.py"
 
-$python $MAIN
+LOG_DIR=/var/log/monitoring
+
+$python $MAIN 1>>$LOG_DIR/monitoring.log 2>>$LOG_DIR/error.log
